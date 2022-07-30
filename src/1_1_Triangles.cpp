3a8e7aa761b8f8e0a154840d331baa25181be59b
zhangzz <924204630@qq.com>
2022-07-30 18:37:32 +0800

    项目配置

---
+++ src/1_1_Triangles.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 92420 on 2022/7/30.
+//
+#include <iostream>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+int main()
+{
+#pragma region OpenGL Initialization
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+    glfwTerminate();
+}
